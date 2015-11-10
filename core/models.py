f507a08884c68c71de7f59b5f136da68fc117334
Amanda <amanda.maturo@quinnipiac.edu>
2015-11-10 19:36:19 +0000

    Add question create view

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Salon(models.Model):
+  salon_name = models.CharField(max_length=300)
+  description = models.TextField(null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  user = models.ForeignKey(User)
+
+  def __unicode__(self):
+    return self.title
--- core/views.py
+++ core/views.py
@@ -4,3 +4,17 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
   template_name = "home.html"
+
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
+from .models import*
+
+class SalonCreateView(CreateView):
+  model = Salon
+  template_name = "salon/salon_form.html"
+  fields = ['salon_name', 'description']
+  success_url = reverse_lazy('home')
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super(SalonCreateView, self).form_valid(form)
